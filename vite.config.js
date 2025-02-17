a23c2665fe30e887a30da707c29a09d0a71a68a3
Shah <shahkabir2003@gmail.com>
2025-02-17 17:00:17 -0500

    Doing again

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react(),
-    createHtmlPlugin({
-        minify: true,
-        pages: {
-          '/hourly.html': {
-            template: 'public/hourly.html'
-          }
-        }
-      })
-  ],
+  plugins: [react()],
   base: '/',
   build: {
     outDir: 'dist',
